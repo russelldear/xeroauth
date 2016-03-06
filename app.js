37733833a255a60068fd552d7f59632c75cdce63
Russell Dear <you@example.com>
2016-03-06 17:25:41 +1300

    Config settings

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+
+var config = require('./config.json')
 var OAuth = require('oauth');
 var open = require('open');
+var events = require('events');
+
+var razor = new events.EventEmitter();
+razor.on('requestTokenRetrieved', verifyAuthorisation);
 
 var oauth = new OAuth.OAuth(
-	'https://api.xero.com/oauth/RequestToken',
-	'https://api.xero.com/oauth/AccessToken',
-	'9Y1R7TLMO2WFQW5WKIGXMKHQE3NK9X',
-	'WJYOSP4QYX4QFBYPYHI8MJ1D5Y2VPC',
+	config.requestUri,
+	config.accessUri,
+	config.consumerKey,
+	config.consumerSecret,
 	'1.0A',
 	null,
 	'HMAC-SHA1'
@@ -17,6 +23,7 @@ var accessToken = "";
 var accessTokenSecret = "";
 var verifier = "";
 
+console.log("Retrieving request token...")
 oauth.getOAuthRequestToken(function(error, request_token, request_token_secret, requestResults){
 	if(error) {
 		console.log('error :' + error)
@@ -24,37 +31,37 @@ oauth.getOAuthRequestToken(function(error, request_token, request_token_secret, 
 	else { 
 		console.log('oauth_token :' + request_token)
 		console.log('oauth_token_secret :' + request_token_secret)
-		console.log('results :' + requestResults)
 
 		requestToken = request_token;
 		requestTokenSecret = request_token_secret;
 
-		open("https://app.xero.com/oauth/APIAuthorise?oauth_token=" + request_token)
+		open(config.authoriseUri + request_token)
 
 		console.log('Enter verification code:')
+
+		razor.emit('requestTokenRetrieved');
 	}
 });
 
-// Go get verification code: https://app.xero.com/oauth/APIAuthorise?oauth_token=OAUTHTOKENFROMPREVIOUS
+function verifyAuthorisation(){
+	var stdin = process.openStdin();
 
-var stdin = process.openStdin();
-
-stdin.addListener("data", function(d) {
-    verifier = d.toString().trim();
-    getAccessToken();
-    process.stdin.pause();
-  });
-
-function getAccessToken() {
-	oauth.getOAuthAccessToken(requestToken, requestTokenSecret, verifier, function(error, access_token, access_token_secret, accessResults) {
-		console.log('access_token :' + access_token)
-		console.log('access_token_secret :' + access_token_secret)
-		console.log('results :' + accessResults)
-		
-		var output = "";
-
-		oauth.getProtectedResource("https://api.xero.com/api.xro/2.0/organisation", "GET", access_token, access_token_secret,  function (error, output, response) {
-			console.log(output);
-		});
+	stdin.addListener("data", function(d) {
+		verifier = d.toString().trim();
+		getAccessToken();
+		process.stdin.pause();
 	});
+
+	function getAccessToken() {
+		oauth.getOAuthAccessToken(requestToken, requestTokenSecret, verifier, function(error, access_token, access_token_secret, accessResults) {
+			console.log('access_token :' + access_token)
+			console.log('access_token_secret :' + access_token_secret)
+
+			var output = "";
+
+			oauth.getProtectedResource("https://api.xero.com/api.xro/2.0/organisation", "GET", access_token, access_token_secret,  function (error, output, response) {
+				console.log(output);
+			});
+		});
+	}
 }
