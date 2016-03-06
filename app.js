d2a8a60348a7bf8b17629448763affc6bcf60411
Russell Dear <you@example.com>
2016-03-06 13:40:32 +1300

    Interactive process

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ var oauth = new OAuth.OAuth(
 	'HMAC-SHA1'
 	);
 
+var requestToken = "";
+var requestTokenSecret = "";
+var accessToken = "";
+var accessTokenSecret = "";
+var verifier = "";
 
 oauth.getOAuthRequestToken(function(error, request_token, request_token_secret, requestResults){
 	if(error) {
@@ -21,20 +26,35 @@ oauth.getOAuthRequestToken(function(error, request_token, request_token_secret, 
 		console.log('oauth_token_secret :' + request_token_secret)
 		console.log('results :' + requestResults)
 
+		requestToken = request_token;
+		requestTokenSecret = request_token_secret;
+
 		open("https://app.xero.com/oauth/APIAuthorise?oauth_token=" + request_token)
+
+		console.log('Enter verification code:')
 	}
 });
 
 // Go get verification code: https://app.xero.com/oauth/APIAuthorise?oauth_token=OAUTHTOKENFROMPREVIOUS
 
-oauth.getOAuthAccessToken('OAUTHTOKENKEYFROMPREVIOUS', 'OAUTHTOKENSECRETFROMPREVIOUS', 'VERIFICATIONCODE', function(error, access_token, access_token_secret, accessResults) {
-	console.log('access_token :' + access_token)
-	console.log('access_token_secret :' + access_token_secret)
-	console.log('results :' + accessResults)
-	
-	var output = "";
+var stdin = process.openStdin();
 
-	oauth.getProtectedResource("https://api.xero.com/api.xro/2.0/organisation", "GET", access_token, access_token_secret,  function (error, output, response) {
-		console.log(output);
+stdin.addListener("data", function(d) {
+    verifier = d.toString().trim();
+    getAccessToken();
+    process.stdin.pause();
+  });
+
+function getAccessToken() {
+	oauth.getOAuthAccessToken(requestToken, requestTokenSecret, verifier, function(error, access_token, access_token_secret, accessResults) {
+		console.log('access_token :' + access_token)
+		console.log('access_token_secret :' + access_token_secret)
+		console.log('results :' + accessResults)
+		
+		var output = "";
+
+		oauth.getProtectedResource("https://api.xero.com/api.xro/2.0/organisation", "GET", access_token, access_token_secret,  function (error, output, response) {
+			console.log(output);
+		});
 	});
-});
+}
